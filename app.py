1fd9899bc7ee3e7ce66d1982d383339e5b3adea1
Myspaceplay <pedropruebas20300@gmail.com>
2025-05-21 20:55:41 -0500

    nuevo cambio en app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import aiRouter  # o la ruta donde tienes tu router
+from routers import aiRouter  # Asegúrate de tener el import correcto
 
 app = FastAPI()
 
-# Habilitar CORS
+# Agrega aquí el dominio del frontend local o remoto que quieras permitir
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontai-vpb6.onrender.com" , "http://127.0.0.1:5500"],  # O especifica ["http://127.0.0.1:5500"] para mayor seguridad
+    allow_origins=["https://frontai-vpb6.onrender.com"], # https://frontai-vpb6.onrender.com
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Incluir rutas
-app.include_router(aiRouter.router)
-
-@app.get("/")
-def index():
-    return {"message": "API running"}
-
+app.include_router(aiRouter)
 
