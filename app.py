7b0e7ae481d61341530fc451639b43bd42838cf2
Myspaceplay <pedropruebas20300@gmail.com>
2025-05-21 20:41:31 -0500

    cambio allow_origins

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Habilitar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # O especifica ["http://127.0.0.1:5500"] para mayor seguridad
+    allow_origins=["https://frontai-vpb6.onrender.com" , "http://127.0.0.1:5500"],  # O especifica ["http://127.0.0.1:5500"] para mayor seguridad
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
